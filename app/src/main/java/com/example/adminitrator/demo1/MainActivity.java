9a1859711308e67100fe2137693348e25f2ef9f8
Pranesh <pranesh.msc@gmail.com>
2017-02-17 11:03:18 +0530

    fgsd

--- app/src/main/java/com/example/adminitrator/demo1/MainActivity.java
+++ app/src/main/java/com/example/adminitrator/demo1/MainActivity.java
@@ -23,5 +23,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),edt.getText()+"", Toast.LENGTH_LONG).show();
             }
         });
+        /*
+        
+         */
     }
 }

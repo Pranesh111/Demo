5fad7afe1534db65e8ef70ee2240ecc529a187a9
Pranesh <pranesh.msc@gmail.com>
2017-02-17 11:11:06 +0530

    sdag

--- app/src/main/java/com/example/adminitrator/demo1/MainActivity.java
+++ app/src/main/java/com/example/adminitrator/demo1/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.adminitrator.demo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button2);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"PP", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }

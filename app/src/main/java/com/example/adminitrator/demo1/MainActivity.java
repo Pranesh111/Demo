265b21e3652f3aa02ec811f9a9fd049aebe20ae6
Pranesh <pranesh.msc@gmail.com>
2017-02-17 10:34:33 +0530

    Commit the branch 2 features

--- app/src/main/java/com/example/adminitrator/demo1/MainActivity.java
+++ app/src/main/java/com/example/adminitrator/demo1/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.adminitrator.demo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(Button) findViewById(R.id.button);
+        edt=(EditText)findViewById(R.id.editText);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),edt.getText()+"", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

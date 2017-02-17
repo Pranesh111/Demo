a9b6df1651a000c98c38173b7f90c0d800383428
Pranesh <pranesh.msc@gmail.com>
2017-02-17 10:40:22 +0530

    master bug fixed & pushed

---
+++ app/src/main/java/com/example/adminitrator/demo1/Test.java
@@ -0,0 +1,11 @@
+package com.example.adminitrator.demo1;
+
+/**
+ * Created by Adminitrator on 2/17/2017.
+ */
+
+public class Test {
+    public static void main(String args[]){
+        
+    }
+}
